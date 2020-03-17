3518759dae133ff705f26cd11136c47f349d7fe4
Max <JKcamus@163.com>
2020-03-17 14:33:56 +0800

    cart 价格计算bug修复，cart completed

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -2,25 +2,18 @@
 const app = getApp()
 
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     cartList: [],
     isSelectAll: true,
     totalPrice: 0,
     totalCounter: 0
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  onLoad() {
     // 1.第一次加载数据
     this.setData({
       cartList: app.globalData.cartList
     })
+
     // 2.设置回调
     app.addCartCallback = () => {
       this.setData({
@@ -28,16 +21,20 @@ Page({
       })
       this.changeData()
     }
+
     // 3.设置修改某个商品的回调
     app.changeGoodsState = (index, goods) => {
+      // 1.修改某一项的选中状态
       this.setData({
         [`cartList[${index}]`]: goods
       })
+
+      // 2.修改全部选中的状态
       const selectAll = !this.data.cartList.find(item => !item.checked)
       this.setData({
         isSelectAll: selectAll
       })
-      
+      this.changeData()
     }
   },
   onShow() {
@@ -48,8 +45,8 @@ Page({
     this.changeData()
   },
   onSelectAll() {
-    // 1.判断是是否全部选中
-    if (this.data.isSelectAll) {
+    // 1.判断是否是全部选中
+    if (this.data.isSelectAll) { // 目前全部选中
       // 全选时改为false,通过遍历将全部改为false
       this.data.cartList.forEach(item => {
         item.checked = false
@@ -58,9 +55,8 @@ Page({
         cartList: this.data.cartList,
         isSelectAll: false
       })
-
     } else {
-      // 当项未勾选时
+      // 当存在未勾选时
       this.data.cartList.forEach(item => {
         item.checked = true
       })
@@ -69,69 +65,27 @@ Page({
         isSelectAll: true
       })
     }
+    // 全选修改数量及价格
+    this.changeData()
   },
   changeData() {
-    let totalPrice = 0
-    let counter = 0
+    // 1.获取所有选中数据
+    let totalPrice = 0;
+    let counter = 0;
+
     for (let item of this.data.cartList) {
       if (item.checked) {
         counter++
         totalPrice += item.price * item.count
       }
     }
+
     console.log(counter, totalPrice)
+
     // 2.修改数据
     this.setData({
       totalCounter: counter,
       totalPrice: totalPrice
     })
-  },
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
   }
 })
